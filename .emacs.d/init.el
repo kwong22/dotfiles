948cd7c718c784bb5a5484df47b003f49289b2a2
Kelvin Wong <kwong691@gmail.com>
2014-06-14 23:05:09 +0000

    First commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,33 @@
+;; -*- mode: emacs-lisp -*-
+;; Simple .emacs configuration
+
+;; ---------------------
+;; -- Global Settings --
+;; ---------------------
+(add-to-list 'load-path "~/.emacs.d")
+(require 'cl)
+(require 'whitespace)
+(require 'compile)
+
+;; ------------
+;; -- Macros --
+;; ------------
+(load "defuns-config.el")
+
+;; ---------------------------
+;; -- JS Mode configuration --
+;; ---------------------------
+(load "js-config.el")
+(add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
+(require 'sws-mode)
+(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; Create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
---
+++ README.md
@@ -0,0 +1,12 @@
+dotfiles
+========
+
+Clone and run on a new Ubuntu machine to configure `emacs` and `tmux`
+as follows:
+
+```sh
+cd $HOME
+git clone https://github.com/kwong22/dotfiles.git
+ln -s dotfiles/.tmux.conf .
+ln -s dotfiles/.emacs.d .
+```
